248b0eabfe6fe0953e43015bc05a0d8d428de48d
otikna <ot@anadolu.edu.tr>
2018-06-21 17:36:10 +0300

    Add files via upload

---
+++ App.py
@@ -0,0 +1,22 @@
+import requests
+
+baslik = {
+'Authorization': 'Token Your-Token',
+'Content-Type': 'application/json'
+}
+
+url = "https://api.cognitus.ai/api/v1/entity"
+
+data=[]
+with open("data.txt", "r", encoding="UTF-8") as f:
+    veri = f.readlines()
+    for s in veri:
+        data.append(s)
+
+for s in data:
+    response = requests.post(url, json={"text":s}, headers=baslik).json()
+    for i in response:
+        if i[1] == "I-LOC":
+            with open("result.txt", "a", encoding="UTF-8") as f:
+                f.write("\n"+i[0])
+
---
+++ Twee.py
@@ -0,0 +1,30 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import json
+import pandas as pd
+import tweepy
+
+arr = []
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = "Your-Access-Token"
+access_token_secret = "Your-Access-Token-Secret"
+consumer_key = "Your-Consumer-Key"
+consumer_secret = "Your-Consumer-Secret"
+
+giris = tweepy.OAuthHandler(consumer_key, consumer_secret)
+giris.set_access_token(access_token, access_token_secret)
+api = tweepy.API(giris)
+
+for tweet in tweepy.Cursor(api.search,
+                           q="secim AND hile",
+                           # Since="2016-08-09",
+                           # until="2014-02-15",
+                           lang="tr",
+                           tweet_mode="extended").items(5000000):
+
+    if 'RT @' not in tweet.full_text:
+        with open("data.txt", "a", encoding="UTF-8") as f:
+            print(tweet.full_text)
+            f.writelines((" ".join(str(x) for x in tweet.full_text.replace('\n', '.').replace('.', ' ').split(" ")[:-1]))+"\n")
